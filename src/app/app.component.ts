8be06eb351302da79a8c7bdf6668343af75a6db2
Bam49eng <bam48minecraft@gmail.com>
2024-09-19 00:32:39 +0000

    mat-table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { CommonModule } from '@angular/common';
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule, MatMenuModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  visibleDivId: string = 'home';  // Initialize an empty string for visibility control
+
+  // Function to show a specific div by ID and hide all others
+  toggleDiv(divId: string): void {
+    this.visibleDivId = divId;  // Set the visible div's ID
+  }
+
+  // Function to check if a div should be visible
+  isDivVisible(divId: string): boolean {
+    return this.visibleDivId === divId;
+  }
   title = 'bcsBees';
 }
